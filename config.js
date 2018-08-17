56f1ce641314d225b0d87f58d2eebb6ba86e60c6
Oakware <i.o.kondratuk@gmail.com>
2018-08-17 15:39:17 +0300

    Updated interface

--- config.js
+++ config.js
@@ -2,11 +2,12 @@ const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const devMode  = 'development';
 const prodMode = 'production';
 
-let features = {
+const features = {
     'babel': (config, mode) => addJsLanguage(config, {
         extensions: [ '.js', '.jsx' ],
         rule: {
@@ -61,14 +62,37 @@ let features = {
         extensions: [ '.styl' ],
         test: /\.styl$/,
         loader: 'stylus-loader'
+    }),
+    'url': (config, mode) => addJsLanguage(config, {
+        rule: {
+            test: /\.(png|jpg|gif|svg)$/i,
+            use: [{
+                loader: 'url-loader',
+                options: { limit: 8192 }
+            }]
+        }
     })
 };
 
+const entries = {
+    index: {
+        js: './src/index/index.coffee',
+        html: './src/template.html',
+        path: 'index.html'
+    },
+    projects: {
+        js: './src/projects/index.jsx',
+        html: './src/template.html',
+        path: 'projects/index.html'
+    }
+};
+
 function addJsLanguage(config, lang) {
-    config.resolve.extensions.push(...lang.extensions);
-    config.module.rules.push(lang.rule);
+    if (lang.extensions)
+        config.resolve.extensions.push(...lang.extensions);
     if (lang.plugins)
         config.plugins.push(...lang.plugins);
+    config.module.rules.push(lang.rule);
 }
 
 function addCssLanguage(config, mode, lang) {
@@ -97,51 +121,47 @@ function addFeatures(config, mode, useFeatures) {
     }
 }
 
+function addEntries(config, entries) {
+    for (let entryName in entries) {
+        let entry = entries[entryName];
+        config.entry[entryName] = entry.js;
+        config.plugins.push(new HtmlWebpackPlugin({
+            template: entry.html,
+            filename: entry.path,
+            chunks: [ entryName ],
+            inject: 'head'
+        }));
+    }
+}
+
 function getWebpackConfig(mode) {
     mode = mode || process.env.NODE_ENV || devMode;
     let buildDir = 'build';
     let useFeatures = ['babel', 'coffeescript', 'css', 'sass'];
 
     let config = {
+        mode: mode,
         context: __dirname,
-        entry: {
-            index: './src/index/index.coffee',
-            projects: './src/projects/index.jsx'
-        },
+        entry: {},
         output: {
             path: path.resolve(__dirname, buildDir),
             filename: 'js/[name].bundle.js'
-        },
-        mode: mode,
-        devServer: {
-            contentBase: './' + buildDir,
-            compress: true,
-            port: 9000
         },
         resolve: { extensions: [] },
         module: { rules: [] },
         plugins: [],
         optimization: {
             splitChunks: { chunks: 'all' }
+        },
+        devServer: {
+            contentBase: './' + buildDir,
+            compress: true,
+            port: 9000
         }
     };
 
     addFeatures(config, mode, useFeatures);
-
-    config.plugins.push(
-        new HtmlWebpackPlugin({
-            template: './src/index/index.html',
-            filename: 'index.html',
-            chunks: [ 'index' ],
-            inject: 'head'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/projects/index.html',
-            filename: 'projects/index.html',
-            chunks: [ 'projects' ],
-            inject: 'head'
-        })
-    );
+    addEntries(config, entries);
 
     if (mode === devMode) {
         config.devtool = 'inline-source-map';
@@ -150,6 +170,10 @@ function getWebpackConfig(mode) {
             filename: "css/[name].bundle.css",
         }));
     }
+
+    config.plugins.push(new CopyWebpackPlugin([
+        { from: 'src/resources', to: 'resources'}
+    ]));
 
     return config;
 }
--- src/index/index.coffee
+++ src/index/index.coffee
@@ -4,21 +4,50 @@ import ReactDOM from 'react-dom'
 import '../core/core.js'
 import './index.scss'
 
+Header = (props) -> (
+    <header className="hero is-medium is-primary is-bold">
+        <div className="hero-head">
+            <nav className="navbar">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a className="navbar-item is-size-4" href="/">
+                            Oakware
+                        </a>
+                    </div>
+                    <div className="navbar-menu">
+                        <div className="navbar-end">
+                            <a className="navbar-item" href="/projects">
+                                Projects
+                            </a>
+                            <a className="navbar-item" href="/about">
+                                About
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div className="hero-body">
+            <div className="container has-text-centered">
+                <h1 className="title">
+                    kondratuk.io
+                </h1>
+                <h2 className="subtitle">
+                    Large subtitle
+                </h2>
+            </div>
+        </div>
+
+        <div className="hero-foot">
+        </div>
+    </header>
+)
+
 export default class App extends React.Component
     render: -> (
         <div>
-            <header className="hero is-medium is-primary is-bold">
-                <div className="hero-body">
-                    <div className="container">
-                        <h1 className="title">
-                            Large title
-                        </h1>
-                        <h2 className="subtitle">
-                            Large subtitle
-                        </h2>
-                    </div>
-                </div>
-            </header>
+            <Header/>
             <main className="section">
                 <div className="container">
                     <h1 className="title">
